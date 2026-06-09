0a8b52314a31e05c3fd50ec085bee3d3e929536c
Luis Azañero <u18310462@utp.edu.pe>
2026-06-08 21:51:22 -0500

    Revert "Merge branch 'master' of https://github.com/LuisAzanero/AppProyectoIntegrador1Grupo1"
    This reverts commit 27aa99e9350240fd0464942f22c7cfb230a71ef4, reversing
    changes made to f3e439c8d593f950387fd05981d0dddc88dd6c2d.

--- src/main/java/pe/edu/utp/transvisa/persistence/VehiculoRepository.java
+++ src/main/java/pe/edu/utp/transvisa/persistence/VehiculoRepository.java
@@ -1,8 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
 package pe.edu.utp.transvisa.persistence;
 
 import pe.edu.utp.transvisa.domain.Vehiculo;
 
+/**
+ *
+ * @author luisazanero
+ */
 public interface VehiculoRepository {
+
     Vehiculo buscarPorId(int idVehiculo);
+
     void actualizarEstadoYKilometraje(Vehiculo vehiculo);
 }
